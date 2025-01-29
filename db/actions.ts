b40324edc7ac6a8eed3732f670e0f1f817768667
Demian Voksi <demian.voksi@gmail.com>
2025-01-29 20:47:25 +0100

    initial commit

---
+++ db/actions.ts
@@ -0,0 +1,43 @@
+'use server';
+
+import { db } from '@/db/drizzle';
+import { todo } from '@/db/schema';
+import { eq, not } from 'drizzle-orm';
+import { revalidatePath } from 'next/cache';
+
+export async function fetchData() {
+	const data = await db.select().from(todo);
+	return data;
+}
+
+export async function addTask(id: number, text: string) {
+	await db.insert(todo).values({
+		id: id,
+		text: text,
+	});
+}
+
+export async function deleteTask(id: number) {
+	await db.delete(todo).where(eq(todo.id, id));
+	revalidatePath('/');
+}
+
+export async function toggleDone(id: number) {
+	await db
+		.update(todo)
+		.set({
+			done: not(todo.done),
+		})
+		.where(eq(todo.id, id));
+	revalidatePath('/');
+}
+
+export async function editTodo(id: number, text: string) {
+	await db
+		.update(todo)
+		.set({
+			text: text,
+		})
+		.where(eq(todo.id, id));
+	revalidatePath('/');
+}
---
+++ migrations/0002_curious_malcolm_colcord.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "todo" ALTER COLUMN "text" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "todo" ADD COLUMN "author" text;
